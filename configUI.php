2c778b31661768afd6892fa7c90cef512fe50188
Bryan Brandau <agent462>
2025-11-21 23:31:55 -0600

    fix alignment of config buttons

--- configUI.php
+++ configUI.php
@@ -383,16 +383,16 @@ if (!in_array($config['networkAdapter'], $interfaces)) {
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <!-- Form Actions -->
-                <div class="formActions">
-                    <button type="button" class="buttons btn-outline-secondary" onclick="window.location.reload();">
-                        <i class="fas fa-undo"></i> Reset
-                    </button>
-                    <button type="submit" name="save_settings" class="buttons btn-success">
-                        <i class="fas fa-save"></i> Save Settings
-                    </button>
-                </div>
+            <!-- Form Actions -->
+            <div class="formActions">
+                <button type="button" class="buttons btn-outline-secondary" onclick="window.location.reload();">
+                    <i class="fas fa-undo"></i> Reset
+                </button>
+                <button type="submit" name="save_settings" class="buttons btn-success">
+                    <i class="fas fa-save"></i> Save Settings
+                </button>
             </div>
         </form>
     </div>
