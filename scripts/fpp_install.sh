e5f133c935c82652a3b3c5cdaa01c7fffd462c25
Bryan Brandau <agent462>
2025-11-23 08:49:16 -0600

    add collectd service file , nice process, install logic

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -5,8 +5,17 @@ apt-get update
 apt-get -y install collectd-core rrdtool
 sudo systemctl disable --now collectd.service
 
-# Copy our custom collectd configuration
+# Copy our custom collectd service file.  We are setting a Nice on the service.
 PLUGIN_DIR="/home/fpp/media/plugins/fpp-plugin-watcher"
+if [ -f "${PLUGIN_DIR}/config/collectd.service" ]; then
+    echo "Installing custom collectd service file..."
+    cp "${PLUGIN_DIR}/config/collectd.service" /lib/systemd/system/collectd.service
+    echo "Collectd service file installed successfully"
+else
+    echo "WARNING: Custom collectd.service not found at ${PLUGIN_DIR}/config/collectd.service"
+fi
+
+# Copy our custom collectd configuration
 if [ -f "${PLUGIN_DIR}/config/collectd.conf" ]; then
     echo "Installing custom collectd configuration..."
     cp "${PLUGIN_DIR}/config/collectd.conf" /etc/collectd/collectd.conf
@@ -20,9 +29,7 @@ fi
 
 # Add required Apache CSP (Content-Security-Policy allowed domains
 # Possible Keys are: 'default-src', 'connect-src', 'img-src', 'script-src', 'style-src', 'object-src'
-# Examples: 
-# ${FPPDIR}/scripts/ManageApacheContentPolicy.sh add connect-src https://domaintotrust.co.uk
-# ${FPPDIR}/scripts/ManageApacheContentPolicy.sh add img-src https://anotherdomain.com
 ${FPPDIR}/scripts/ManageApacheContentPolicy.sh add script-src https://cdn.jsdelivr.net
 
+# Set FPP to restart to apply changes
 setSetting restartFlag 1
