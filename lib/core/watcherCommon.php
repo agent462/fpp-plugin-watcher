0c7d552ee9a382783aa5e8076d7b04c8257c8ffc
agent462 <agent462@gmail.com>
2025-12-15 17:31:31 -0600

    move collectd data files so we don't lose them in an OS update

--- lib/core/watcherCommon.php
+++ lib/core/watcherCommon.php
@@ -644,7 +644,7 @@ function getDataCategories() {
         ],
         'collectd' => [
             'name' => 'Collectd RRD Data',
-            'dir' => '/var/lib/collectd/rrd',
+            'dir' => '/home/fpp/media/plugin-data/fpp-plugin-watcher/collectd/rrd',
             'description' => 'System metrics collected by collectd (CPU, memory, disk, etc.)',
             'showFiles' => false,
             'recursive' => true,
